c9573c8699c6aa35e48dbcad816968c6057d1d48
khizarkhizar <khizarrana756@gmail.com>
2020-05-18 00:14:58 +0500

    generate token of user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 const User = require("./../modals/userModel");
-
+const jwt = require("jsonwebtoken");
 exports.signup = async (req, res, next) => {
   const newUser = await User.create(req.body);
-
-  res.status(200).json({ status: "success", data: { user: newUser } });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  res.status(200).json({ status: "success", token, data: { user: newUser } });
 };
